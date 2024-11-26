d4c4b86da3784e635d2f8b63f45fd8fd5bc69cb5
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-27 00:13:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Redirect to the next page after 10 seconds (when progress bar is complete)
 setTimeout(() => {
     window.location.href = "https://your-next-page-url.com"; // Replace with your desired URL
-}, 10000);
+}, 5000);
