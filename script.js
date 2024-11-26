f11c2a89fef8f8e573f208917592e620bba1db31
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-26 21:58:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 // Redirect to the next page after 10 seconds
 setTimeout(() => {
-    window.location.href = "https://your-next-page-url.com"; // Replace with the actual URL
-}, 10000);
+    window.location
